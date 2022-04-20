2ede5605218b2cfa8bd13c5d7d476052467c5b3c
Yoda <jkarl.spinti@gmail.com>
2022-04-20 15:04:30 -0600

    Changing the executable to hips-run

--- run/runManyRlz.sh
+++ run/runManyRlz.sh
@@ -34,7 +34,7 @@ while [ $it -lt $nRlz ] ; do
     echo ""
     echo "-------------------- REALIZATION  $it -----------------------"
     echo ""
-    ./sec.x $caseName $it
+    ./hips_run $caseName $it
     it=$(($it + 1))
 done
 
--- run/runOneRlz.sh
+++ run/runOneRlz.sh
@@ -28,7 +28,7 @@ runCase () {
 
     echo "*** RUNNING ***"
     echo "Output is being written to ../$caseName/runtime/runtime_* and ../$caseName/data"
-    ./sec.x $caseName 0          # 0 is the shift (realization # here)
+    ./hips-run $caseName 0          # 0 is the shift (realization # here)
 
 }
 
