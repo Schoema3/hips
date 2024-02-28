6546a6804e334907fde5246bd1ff57a6e867ad5e
Masoomeh <m.behrang2015@gmail.com>
2024-02-27 22:06:43 -0700

    changes in cmake

---
+++ build/src/CMakeFiles/hips.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mbehrang/hips-folders/hips-build/hips/src/batchReactor_cantera.cc" "src/CMakeFiles/hips.dir/batchReactor_cantera.cc.o" "gcc" "src/CMakeFiles/hips.dir/batchReactor_cantera.cc.o.d"
+  "/Users/mbehrang/hips-folders/hips-build/hips/src/batchReactor_cvode.cc" "src/CMakeFiles/hips.dir/batchReactor_cvode.cc.o" "gcc" "src/CMakeFiles/hips.dir/batchReactor_cvode.cc.o.d"
+  "/Users/mbehrang/hips-folders/hips-build/hips/src/hips.cc" "src/CMakeFiles/hips.dir/hips.cc.o" "gcc" "src/CMakeFiles/hips.dir/hips.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/hips.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hips.dir/batchReactor_cantera.cc.o"
+  "CMakeFiles/hips.dir/batchReactor_cantera.cc.o.d"
+  "CMakeFiles/hips.dir/batchReactor_cvode.cc.o"
+  "CMakeFiles/hips.dir/batchReactor_cvode.cc.o.d"
+  "CMakeFiles/hips.dir/hips.cc.o"
+  "CMakeFiles/hips.dir/hips.cc.o.d"
+  "libhips.a"
+  "libhips.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hips.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
