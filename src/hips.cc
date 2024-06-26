ff6be3c063e2372d553041ef4771e1ad255d3e11
Masoomeh <m.behrang2015@gmail.com>
2024-06-26 01:23:20 -0600

    adding performreaction to block

--- src/hips.cc
+++ src/hips.cc
@@ -760,9 +760,11 @@ void hips::reactParcels_LevelTree(const int iLevel, const int iTree) {
         for (int k=0; k<nsp; k++)
             y[k] = varData[k+1][0][ime];
 #ifdef REACTIONS_ENABLED
+        if(performReaction) {
                 bRxr->react(h, y, dtEE);
-                varRho[ime] =  bRxr->getDensity();             //Mb
+                varRho[ime] =  bRxr->getDensity();            
             Temp[ime] = bRxr->temperature;
+        }
 #endif
         varData[0][0][ime] = h;
         for (int k=0; k<nsp; k++)
